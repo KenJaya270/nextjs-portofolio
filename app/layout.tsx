7393e0546a01395ae26ef452f846163623b086a9
KenJaya270 <kenjayakusuma36@smk.belajar.id>
2023-05-13 18:16:53 +0800

    docs: fix error while deploying

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "../styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import Head from './head'
 import { ThemeProvider } from "next-themes"
 
 export default function RootLayout({
@@ -15,7 +16,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <Head />
       <body className="bg-white">
           <Navbar />
           {children}
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,86 +1,86 @@
-// "use client"
-// import React from 'react';
-// import { useForm, ValidationError } from '@formspree/react';
-// function ContactForm() {
-//     const [state, handleSubmit] = useForm("myyaonoq");
-//     if (state.succeeded) {
-//         return <p>Thanks For Messaging Me.</p>
-//     }
-//     return (
-//         <div>
-//             <div className="mb-3">
-//                 <h2 className='text-black font-bold text-3xl tracking-[0.3rem] my-3'>Contact Me</h2>
-//                 <p className='text-2xl'>If you have any question, just fill form below. I will read your message as soon as possible.</p>
-//             </div>
-//             <div className="rounded py-10 mt-1">
-//                 <form onSubmit={handleSubmit}>
-//                     <div>
-//                         <label className='text-gray-700 block mb-1 font-bold' htmlFor="email">Email Address</label>
-//                         <input id="email" type="email" name="email" className='w-full h-12 mb-5 rounded px-4 py-4 bg-transparent border-2 border-slate-950 text-black focus:outline-none'/>
-//                         <ValidationError prefix="Email" field="email" errors={state.errors}/>
-//                     </div>
-//                     <label className='text-gray-700 block font-bold mb-1' htmlFor="message">Message</label>
-//                     <textarea id="message" name="message" className='h-200 w-full rounded px-4 py-4 bg-transparent border-2 border-slate-900 text-black focus:outline-none'/>
-//                     <ValidationError 
-//                         prefix="Message" 
-//                         field="message"
-//                         errors={state.errors}
-//                     />
-//                     <button className='bg-[#19A7CE] text-white px-5 py-2 rounded-lg hover:bg-transparent hover:text-[#19A7CE] hover:border-2 hover:border-[#19A7CE] font-semibold transition-colors' id='submit' type="submit" disabled={state.submitting}>
-//                         Submit
-//                     </button>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-// function App() {
-//     return (
-//         <ContactForm />
-//     );
-// }
-// export default App;
-
+"use client"
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 function ContactForm() {
-  const [state, handleSubmit] = useForm("xvonyale");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
-  return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-  );
+    const [state, handleSubmit] = useForm("xvonyale");
+    if (state.succeeded) {
+        return <p>Thanks For Messaging Me.</p>
+    }
+    return (
+        <div>
+            <div className="mb-3">
+                <h2 className='text-black font-bold text-3xl tracking-[0.3rem] my-3'>Contact Me</h2>
+                <p className='text-2xl'>If you have any question, just fill form below. I will read your message as soon as possible.</p>
+            </div>
+            <div className="rounded py-10 mt-1">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label className='text-gray-700 block mb-1 font-bold' htmlFor="email">Email Address</label>
+                        <input id="email" type="email" name="email" className='w-full h-12 mb-5 rounded px-4 py-4 bg-transparent border-2 border-slate-950 text-black focus:outline-none'/>
+                        <ValidationError prefix="Email" field="email" errors={state.errors}/>
+                    </div>
+                    <label className='text-gray-700 block font-bold mb-1' htmlFor="message">Message</label>
+                    <textarea id="message" name="message" className='h-200 w-full rounded px-4 py-4 bg-transparent border-2 border-slate-900 text-black focus:outline-none'/>
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button className='bg-[#19A7CE] text-white px-5 py-2 rounded-lg hover:bg-transparent hover:text-[#19A7CE] hover:border-2 hover:border-[#19A7CE] font-semibold transition-colors' id='submit' type="submit" disabled={state.submitting}>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 function App() {
-  return (
-    <ContactForm />
-  );
+    return (
+        <ContactForm />
+    );
 }
 export default App;
+
+// import React from 'react';
+// import { useForm, ValidationError } from '@formspree/react';
+// function ContactForm() {
+//   const [state, handleSubmit] = useForm("xvonyale");
+//   if (state.succeeded) {
+//       return <p>Thanks for joining!</p>;
+//   }
+//   return (
+//       <form onSubmit={handleSubmit}>
+//       <label htmlFor="email">
+//         Email Address
+//       </label>
+//       <input
+//         id="email"
+//         type="email" 
+//         name="email"
+//       />
+//       <ValidationError 
+//         prefix="Email" 
+//         field="email"
+//         errors={state.errors}
+//       />
+//       <textarea
+//         id="message"
+//         name="message"
+//       />
+//       <ValidationError 
+//         prefix="Message" 
+//         field="message"
+//         errors={state.errors}
+//       />
+//       <button type="submit" disabled={state.submitting}>
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
+// function App() {
+//   return (
+//     <ContactForm />
+//   );
+// }
+// export default App;
